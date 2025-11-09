a9175f59aca82049ddd207f0a97171cb27bab29d
mookshitaashmita-png <mookshitaashmita@gmail.com>
2025-11-09 16:14:51 +0400

    Fix static files settings for Render deployment

--- hybridproj/settings.py
+++ hybridproj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +28,6 @@ DEBUG = True
 
 ROOT_URLCONF = 'hybridproj.urls'
 
-import os
 
 TEMPLATES = [
     {
@@ -93,9 +92,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Prototype', 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Prototype'),  # folder where your CSS/JS are located
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
