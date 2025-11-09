3ffe6922347b92e40ff58dcc4428ddd85633e515
mookshitaashmita-png <mookshitaashmita@gmail.com>
2025-11-09 16:27:53 +0400

    Enable WhiteNoise to serve static files on Render

--- hybridproj/settings.py
+++ hybridproj/settings.py
@@ -130,6 +130,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 
